3103da37b2374f3c959dd80907d24d5c47aa2718
Dan Lee <danlee01@ucla.edu>
2021-12-09 20:04:58 -0800

    implemented logging, reformatted code

--- most_active_cookie.py
+++ most_active_cookie.py
@@ -1,15 +1,20 @@
 """
-most_active_cookie is a command line program that prints the most active cookie(s) in a log on a given day. 
+most_active_cookie is a command line program that prints 
+the most active cookie(s) in a log on a given day. 
 We define the most active cookie as one seen the most on a given day
 """
 import argparse
-import pytest
 import logging
 
 
 def get_cookie(log):
     """Return the cookie of a given log"""
-    cookie, _ = log
+    try:
+        cookie, _ = log
+    except ValueError:
+        logging.debug(f"Tuple unpacking failed inside get_cookie. log: {log}")
+        cookie = None
+
     return cookie
 
 
@@ -18,8 +23,12 @@ def get_timestamp(log):
     if not log:
         return log
     
-    _, timestamp = log
-    date, time = timestamp.strip().split("T")
+    try:
+        _, timestamp = log
+        date, time = timestamp.strip().split("T")
+    except ValueError:
+        logging.debug(f"Tuple unpacking failed inside get_timestamp. log: {log}")
+        date, time = None, None
 
     return date, time
 
@@ -31,17 +40,18 @@ def get_most_recent_timestamp(logs, target_date=None):
     """
     if not logs:
         return ""
-    
-    if not target_date:
-        date, time = get_timestamp(logs[0])
-        return date, time
-    else:
-        for log in logs:
-            date, time = get_timestamp(log)
-            if date == target_date:
-                return date, time
-    
-    return -1
+    try:
+        if not target_date:
+            date, time = get_timestamp(logs[0])
+        else:
+            for log in logs:
+                date, time = get_timestamp(log)
+                if date == target_date:
+                    break
+    except ValueError:
+        logging.debug(f"Tuple unpacking failed inside get_most_recent_timestamp. log: {log}")
+        date, time = None, None
+    return date, time
 
 
 def most_active_cookie(log, target_date=None):
@@ -52,12 +62,14 @@ def most_active_cookie(log, target_date=None):
     try:
         cookie_log = open(log, "r")
     except IOError:
+        logging.debug("cookie_log could not be opened.")
         return
     
     logs = [log.split(",") for log in cookie_log]
 
     if not target_date:
         target_date, _ = get_most_recent_timestamp(logs)
+        logging.debug(f"User did not provide -d. Default to most recent day: {target_date}")
 
     cookie_counter = {}
     for log in logs:
@@ -69,9 +81,12 @@ def most_active_cookie(log, target_date=None):
             cookie_counter[cookie] += 1
 
     max_occurrances = max(cookie_counter.values())
-    most_active_cookies = [cookie for cookie in cookie_counter.keys() if cookie_counter[cookie] == max_occurrances]
+    most_active = []
+    for cookie in cookie_counter.keys():
+        if cookie_counter[cookie] == max_occurrances:
+            most_active.append(cookie)
 
-    for cookie in most_active_cookies:
+    for cookie in most_active:
         print(cookie)
 
 
@@ -80,5 +95,9 @@ if __name__ == "__main__":
     parser.add_argument("cookie_log", help="cookie file to be searched")
     parser.add_argument("-d", "--date", type=str, help="date in UTC.")
     args = parser.parse_args()
+
+    logging.basicConfig(filename="most_active_cookie.log", \
+                        format='%(levelname)s: %(message)s', \
+                        filemode='w+', level=logging.DEBUG) 
 
     most_active_cookie(args.cookie_log, args.date)
